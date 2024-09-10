c04cdcd51d5cd869d425362aa6e4563be6a6a2e6
Maxime Tellene <56634101+mtellene@users.noreply.github.com>
2024-09-10 17:28:30 +0200

    Create Case_speciale.py

---
+++ BTS_SIO/2e_année/rappels/POO/code/Case_speciale.py
@@ -0,0 +1,8 @@
+class Case_speciale:
+
+    def __init__(self, nom):
+        """
+            Constructeur de Case_spéciale
+            La méthode est faite, pas besoin de la modifier
+        """
+        self.nom = nom
