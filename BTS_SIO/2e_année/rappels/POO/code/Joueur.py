994c07b6852dd810a68474e292f9af16361a634f
Maxime Tellene <56634101+mtellene@users.noreply.github.com>
2024-09-10 17:29:42 +0200

    Create Joueur.py

---
+++ BTS_SIO/2e_année/rappels/POO/code/Joueur.py
@@ -0,0 +1,28 @@
+from random import randint
+
+class Joueur:
+
+    def __init__(self, nom):
+        """
+            Constructeur de Joueur
+        """
+
+    def tirer_de(self):
+        """
+            Renvoie la valeur du dé
+        """
+
+    def deplacement(self, nb_cases):
+        """
+            Déplace le joueur de nb_cases
+        """
+
+    def acheter(self, terrain):
+        """
+            Le joueur achete le terrain terrain
+        """
+
+    def payer(self, terrain, autre_joueur):
+        """
+            Le joueur paye le loyer du terrain terrain à autre_joueur
+        """
