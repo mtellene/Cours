36880de4bd24ef57daff67bc5eb053c697ba69a5
Maxime Tellene <56634101+mtellene@users.noreply.github.com>
2024-09-10 17:32:04 +0200

    Create Jeu.py

---
+++ BTS_SIO/2e_année/rappels/POO/code/Jeu.py
@@ -0,0 +1,18 @@
+from Joueur import Joueur
+from Plateau import Plateau
+from Partie import Partie
+
+from random import randint
+
+if __name__ == "__main__":
+
+    #création de la partie
+
+    #définition du premier joueur
+
+    #boucle de jeu
+
+        #lancer un tour de jeu
+        #changer de joueur
+
+    #afficher le gagnant
