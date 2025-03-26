89ce1b0347bba98bff5540fc7328905c31fda6d1
Maxime Tellene <56634101+mtellene@users.noreply.github.com>
2025-03-26 14:51:06 +0100

    Update choix_profil.php

---
+++ BTS_SIO/1ere_année/Netflux/choix_profil.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+        <link rel="stylesheet" href="style.css">
+        <title>Netflux</title>
+        <link rel="stylesheet" media="screen and (max-width: 1000px)" href="style_telephone.css">
+    </head>
+
+    <body id="page_accueil">
+        <header>
+            <h1>Qui est-ce ?</h1>
+        </header>
+        
+        <div id="profils">
+            <ul>
+                <li>
+                    <a href="profil.html">
+                        <img src="images/profil1.png" alt="profil1">
+                        <span>Pierre</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="profil.html">
+                        <img src="images/profil2.png" alt="profil1">
+                        <span>Thibaut</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="profil.html">
+                        <img src="images/profil3.png" alt="profil1">
+                        <span>Yoann</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="profil.html">
+                        <img src="images/profil4.png" alt="profil1">
+                        <span>Valentin</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <script src="script.js"></script>
+    </body>
+</html>
